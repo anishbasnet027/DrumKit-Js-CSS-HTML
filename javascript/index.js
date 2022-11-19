45a0e9ee7ea6faf8b642b20fdcc4d96d75af65f9
Anish Basnet <anishbasnet027@gmail.com>
2022-11-19 18:57:21 +0545

    drum kit ready css left

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+window.addEventListener('keydown', function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
+    if(!audio) return;
+    audio.currentTime=0; 
+    audio.play();
+    key.classList.add('playing')
+});
+
+function removeTransition(e) {
+
+if (e.propertyName !== 'transform') return;
+ this.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelectorAll('.drum')); 
+// keys.forEach(drum=> drum.addEventListener('transitioned', removeTransition));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+// console.log(keys)
